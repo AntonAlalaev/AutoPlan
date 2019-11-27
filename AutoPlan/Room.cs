1d3de6b39566abdfd2195905a3e2a36a2db83e10
Anton <antonalalaev@gmail.com>
2019-11-27 16:17:55 +0500

    Базовые классы 27/11/2009 добавил препядствия в класс комнат и их обработку (только с теми, которые пересекаются)

--- AutoPlan/Room.cs
+++ AutoPlan/Room.cs
@@ -6,10 +6,50 @@ using System.Threading.Tasks;
 
 namespace AutoPlan
 {
-    class Room:Rectangle
+    class Room : Rectangle
     {
-        public Room(Point BottomLeft, Point BottomRight, Point TopLeft, Point TopRight):base(BottomLeft, BottomRight, TopLeft, TopRight)
+        /// <summary>
+        /// Базовый конструктор по четырем точкам
+        /// </summary>
+        /// <param name="BottomLeft"></param>
+        /// <param name="BottomRight"></param>
+        /// <param name="TopLeft"></param>
+        /// <param name="TopRight"></param>
+        public Room(Point BottomLeft, Point BottomRight, Point TopLeft, Point TopRight) : base(BottomLeft, BottomRight, TopLeft, TopRight)
         {
+            Initialaize();
+        }
+
+        /// <summary>
+        /// Базовый конструктор по двум точкам
+        /// </summary>
+        /// <param name="BottomLeft"></param>
+        /// <param name="TopRight"></param>
+        public Room(Point BottomLeft, Point TopRight) : base(BottomLeft, TopRight)
+        {
+            Initialaize();
+        }
+
+        /// <summary>
+        /// Инициализатор класса
+        /// </summary>
+        private void Initialaize()
+        {
+            Obstacles = new List<Rectangle>();
+        }
+
+        /// <summary>
+        /// Препядствия в виде прямоугольников
+        /// </summary>
+        protected List<Rectangle> Obstacles;
+        
+        /// <summary>
+        /// Добавление препядствий в помещение
+        /// </summary>
+        /// <param name="Obstacles"></param>
+        public void AddObstacles(List<Rectangle> Obstacles)
+        {            
+            this.Obstacles.AddRange(IntersectWith(Obstacles));
         }
 
     }
