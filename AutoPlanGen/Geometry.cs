943dc1219ba87789fb36f0f2c9176ecc12564160
Anton <antonalalaev@gmail.com>
2019-12-04 15:25:41 +0500

    Перенес общие геометрические функции в общий класс, они в дальнейшем будут необходимы в работе.

---
+++ AutoPlanGen/Geometry.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoPlan
+{
+    public class Geometry
+    {
+        /// <summary>
+        /// Длина прямой
+        /// </summary>
+        /// <param name="One">Первая точка</param>
+        /// <param name="Two">Вторая точка</param>
+        /// <returns></returns>
+        public static double LineLength(Point One, Point Two)
+        {
+            return Math.Sqrt((Two.X - One.X) * (Two.X - One.X) + (Two.Y - One.Y) * (Two.Y - One.Y));
+        }
+
+        /// <summary>
+        /// Находит точку пересечения между длиниями
+        /// p1-->p2 и p3-->p4
+        /// </summary>
+        /// <param name="p1">Первая точка первой линии</param>
+        /// <param name="p2">Вторая точка первой линии</param>
+        /// <param name="p3">Первая точка второй линии</param>
+        /// <param name="p4">Вторая точка второй линии</param>
+        /// <param name="lines_intersect">Пересекаются ли линии?</param>
+        /// <param name="segments_intersect">Пересекаются ли заданные сегменеты</param>
+        /// <param name="intersection">точка пересечения</param>
+        /// <param name="close_p1">Ближайшая точка на линии 1</param>
+        /// <param name="close_p2">Ближайшая точка на линии 2</param>
+        public static void FindIntersection(Point p1, Point p2, Point p3, Point p4, out bool lines_intersect, out bool segments_intersect, out Point intersection, out Point close_p1, out Point close_p2)
+        {
+            // Get the segments' parameters.
+            double dx12 = p2.X - p1.X;
+            double dy12 = p2.Y - p1.Y;
+            double dx34 = p4.X - p3.X;
+            double dy34 = p4.Y - p3.Y;
+
+            // Solve for t1 and t2
+            double denominator = (dy12 * dx34 - dx12 * dy34);
+
+            double t1 = ((p1.X - p3.X) * dy34 + (p3.Y - p1.Y) * dx34) / denominator;
+            if (double.IsInfinity(t1))
+            {
+                // The lines are parallel (or close enough to it).
+                lines_intersect = false;
+                segments_intersect = false;
+                intersection = new Point(double.NaN, double.NaN);
+                close_p1 = new Point(double.NaN, double.NaN);
+                close_p2 = new Point(double.NaN, double.NaN);
+                return;
+            }
+            lines_intersect = true;
+
+            double t2 = ((p3.X - p1.X) * dy12 + (p1.Y - p3.Y) * dx12) / -denominator;
+
+            // Find the point of intersection.
+            intersection = new Point(p1.X + dx12 * t1, p1.Y + dy12 * t1);
+
+            // The segments intersect if t1 and t2 are between 0 and 1.
+            segments_intersect = ((t1 >= 0) && (t1 <= 1) && (t2 >= 0) && (t2 <= 1));
+
+            // Find the closest points on the segments.
+            if (t1 < 0)
+            {
+                t1 = 0;
+            }
+            else if (t1 > 1)
+            {
+                t1 = 1;
+            }
+
+            if (t2 < 0)
+            {
+                t2 = 0;
+            }
+            else if (t2 > 1)
+            {
+                t2 = 1;
+            }
+            close_p1 = new Point(p1.X + dx12 * t1, p1.Y + dy12 * t1);
+            close_p2 = new Point(p3.X + dx34 * t2, p3.Y + dy34 * t2);
+        }
+
+        /// <summary>
+        /// Возвращает угол между линиями заданными тремя точками
+        /// </summary>
+        /// <param name="A">Точка А</param>
+        /// <param name="B">Точка В</param>
+        /// <param name="C">Точка С</param>
+        /// <returns></returns>
+        public static double CrossProductLength(Point A, Point B, Point C)
+        {
+            return CrossProductLength(A.X, A.Y, B.X, B.Y, C.X, C.Y);
+        }
+
+        /// <summary>
+        /// Return the cross product AB x BC.
+        /// The cross product is a vector perpendicular to AB
+        /// and BC having length |AB| * |BC| * Sin(theta) and
+        /// with direction given by the right-hand rule.
+        /// For two vectors in the X-Y plane, the result is a
+        /// vector with X and Y components 0 so the Z component
+        /// gives the vector's length and direction.
+        /// </summary>
+        /// <param name="Ax"></param>
+        /// <param name="Ay"></param>
+        /// <param name="Bx"></param>
+        /// <param name="By"></param>
+        /// <param name="Cx"></param>
+        /// <param name="Cy"></param>
+        /// <returns></returns>
+        public static double CrossProductLength(double Ax, double Ay,
+            double Bx, double By, double Cx, double Cy)
+        {
+            // Get the vectors' coordinates.
+            double BAx = Ax - Bx;
+            double BAy = Ay - By;
+            double BCx = Cx - Bx;
+            double BCy = Cy - By;
+
+            // Calculate the Z coordinate of the cross product.
+            return (BAx * BCy - BAy * BCx);
+        }
+
+    }
+}
--- AutoPlanGen/Polygon.cs
+++ AutoPlanGen/Polygon.cs
@@ -15,7 +15,7 @@ namespace AutoPlan
     public class Polygon
     {
         /// <summary>
-        /// Список вершин прямоугольника
+        /// Список вершин многоугольника
         /// </summary>
         protected List<Point> VertexList;
 
@@ -78,6 +78,14 @@ namespace AutoPlan
         }
 
         /// <summary>
+        /// Вершины многоугольнинка
+        /// </summary>
+        public List<Point> Vertices
+        {
+            get { return VertexList; }
+        }
+
+        /// <summary>
         /// Пересекает ли текущий полигон заданный
         /// </summary>
         /// <param name="obj">заданный полигон</param>
@@ -117,12 +125,7 @@ namespace AutoPlan
                 {
                     B = (A + 1) % num_points;
                     C = (B + 1) % num_points;
-
-                    double cross_product =
-                        CrossProductLength(
-                            VertexList[A].X, VertexList[A].Y,
-                            VertexList[B].X, VertexList[B].Y,
-                            VertexList[C].X, VertexList[C].Y);
+                    double cross_product = Geometry.CrossProductLength(VertexList[A], VertexList[B], VertexList[C]);
                     if (cross_product < 0)
                     {
                         got_negative = true;
@@ -133,42 +136,10 @@ namespace AutoPlan
                     }
                     if (got_negative && got_positive) return false;
                 }
-
                 // If we got this far, the polygon is convex.
                 return true;
-
             }
         }
-       
-        /// <summary>
-        /// Return the cross product AB x BC.
-        /// The cross product is a vector perpendicular to AB
-        /// and BC having length |AB| * |BC| * Sin(theta) and
-        /// with direction given by the right-hand rule.
-        /// For two vectors in the X-Y plane, the result is a
-        /// vector with X and Y components 0 so the Z component
-        /// gives the vector's length and direction.
-        /// </summary>
-        /// <param name="Ax"></param>
-        /// <param name="Ay"></param>
-        /// <param name="Bx"></param>
-        /// <param name="By"></param>
-        /// <param name="Cx"></param>
-        /// <param name="Cy"></param>
-        /// <returns></returns>
-        public static double CrossProductLength(double Ax, double Ay,
-            double Bx, double By, double Cx, double Cy)
-        {
-            // Get the vectors' coordinates.
-            double BAx = Ax - Bx;
-            double BAy = Ay - By;
-            double BCx = Cx - Bx;
-            double BCy = Cy - By;
-
-            // Calculate the Z coordinate of the cross product.
-            return (BAx * BCy - BAy * BCx);
-        }
-
 
         /// <summary>
         /// Площадь многоугольника 
@@ -292,7 +263,7 @@ namespace AutoPlan
                 Point pjk2 = new Point(old_points[k].X + n2.X, old_points[k].Y + n2.Y);
 
                 // See where the shifted lines ij and jk intersect.
-                FindIntersection(pij1, pij2, pjk1, pjk2, out bool lines_intersect, out _, out Point poi, out _, out _);
+                Geometry.FindIntersection(pij1, pij2, pjk1, pjk2, out bool lines_intersect, out _, out Point poi, out _, out _);
                 Debug.Assert(lines_intersect, "Edges " + i + "-->" + j + " and " + j + "-->" + k + " are parallel");
                 enlarged_points.Add(poi);
             }
@@ -311,91 +282,15 @@ namespace AutoPlan
                 {
                     if (i == 0)
                         continue;
-                    length += LineLength(VertexList[i - 1], VertexList[i]);
+                    length += Geometry.LineLength(VertexList[i - 1], VertexList[i]);
                 }
                 if (VertexList.Count > 0)
-                    length += LineLength(VertexList[VertexList.Count - 1], VertexList[0]);
+                    length += Geometry.LineLength(VertexList[VertexList.Count - 1], VertexList[0]);
                 return length;
             }
         }
 
-        /// <summary>
-        /// Длина прямой
-        /// </summary>
-        /// <param name="One">Первая точка</param>
-        /// <param name="Two">Вторая точка</param>
-        /// <returns></returns>
-        private static double LineLength(Point One, Point Two)
-        {
-            return Math.Sqrt((Two.X - One.X) * (Two.X - One.X) + (Two.Y - One.Y) * (Two.Y - One.Y));
-        }
 
-        /// <summary>
-        /// Находит точку пересечения между длиниями
-        /// p1-->p2 и p3-->p4
-        /// </summary>
-        /// <param name="p1">Первая точка первой линии</param>
-        /// <param name="p2">Вторая точка первой линии</param>
-        /// <param name="p3">Первая точка второй линии</param>
-        /// <param name="p4">Вторая точка второй линии</param>
-        /// <param name="lines_intersect">Пересекаются ли линии?</param>
-        /// <param name="segments_intersect">Пересекаются ли заданные сегменеты</param>
-        /// <param name="intersection">точка пересечения</param>
-        /// <param name="close_p1">Ближайшая точка на линии 1</param>
-        /// <param name="close_p2">Ближайшая точка на линии 2</param>
-        private static void FindIntersection(Point p1, Point p2, Point p3, Point p4, out bool lines_intersect, out bool segments_intersect, out Point intersection, out Point close_p1, out Point close_p2)
-        {
-            // Get the segments' parameters.
-            double dx12 = p2.X - p1.X;
-            double dy12 = p2.Y - p1.Y;
-            double dx34 = p4.X - p3.X;
-            double dy34 = p4.Y - p3.Y;
-
-            // Solve for t1 and t2
-            double denominator = (dy12 * dx34 - dx12 * dy34);
-
-            double t1 = ((p1.X - p3.X) * dy34 + (p3.Y - p1.Y) * dx34) / denominator;
-            if (double.IsInfinity(t1))
-            {
-                // The lines are parallel (or close enough to it).
-                lines_intersect = false;
-                segments_intersect = false;
-                intersection = new Point(double.NaN, double.NaN);
-                close_p1 = new Point(double.NaN, double.NaN);
-                close_p2 = new Point(double.NaN, double.NaN);
-                return;
-            }
-            lines_intersect = true;
-
-            double t2 = ((p3.X - p1.X) * dy12 + (p1.Y - p3.Y) * dx12) / -denominator;
-
-            // Find the point of intersection.
-            intersection = new Point(p1.X + dx12 * t1, p1.Y + dy12 * t1);
-
-            // The segments intersect if t1 and t2 are between 0 and 1.
-            segments_intersect = ((t1 >= 0) && (t1 <= 1) && (t2 >= 0) && (t2 <= 1));
-
-            // Find the closest points on the segments.
-            if (t1 < 0)
-            {
-                t1 = 0;
-            }
-            else if (t1 > 1)
-            {
-                t1 = 1;
-            }
-
-            if (t2 < 0)
-            {
-                t2 = 0;
-            }
-            else if (t2 > 1)
-            {
-                t2 = 1;
-            }
-            close_p1 = new Point(p1.X + dx12 * t1, p1.Y + dy12 * t1);
-            close_p2 = new Point(p3.X + dx34 * t2, p3.Y + dy34 * t2);
-        }
 
 
     }
